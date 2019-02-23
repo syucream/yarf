6200707468ac2fb5e667ae1a429cada91cf23eb6
Ryo Okubo <syucream1031@gmail.com>
2019-02-23 12:32:02 +0900

    Implement essential features for hello example

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 extern crate libc;
 
-use libc::{c_char, c_int, c_uint, c_ulong, c_void, off_t, size_t, uint64_t};
-
-#[allow(non_camel_case_types)]
+use libc::{c_char, c_int, c_uint, c_ulong, c_void, off_t, size_t, stat, uint64_t};
 
 #[repr(C)]
+#[allow(non_camel_case_types)]
 pub struct fuse_args {
 	argc: c_int,
 	argv: *mut *mut c_char,
@@ -12,6 +11,7 @@ pub struct fuse_args {
 }
 
 #[repr(C)]
+#[allow(non_camel_case_types)]
 pub struct fuse_opt {
 	templ: *const c_char,
 	offset: c_ulong,
@@ -19,6 +19,7 @@ pub struct fuse_opt {
 }
 
 #[repr(C)]
+#[allow(non_camel_case_types)]
 pub struct fuse_file_info {
     pub flag: c_int,
     pub feature_flags: c_uint,
@@ -37,12 +38,33 @@ pub struct fuse_file_info {
 }
 
 #[repr(C)]
-pub struct fuse_operations {
-    // pub getattr: fn(self: *mut fuse_operations, path: *const c_char) -> c_int,
-    pub read: extern fn(path: *const c_char, buf: *mut c_char, size: size_t, offset: off_t, fi: *mut fuse_file_info) -> c_int,
+#[allow(non_camel_case_types)]
+pub enum fuse_readdir_flags {
+    FUSE_READDIR_PLUS = (1 << 1)
 }
 
+#[repr(C)]
+#[allow(non_camel_case_types)]
+pub enum fuse_fill_dir_flags {
+    FUSE_FILL_DIR_PLUS = (1 << 1)
+}
+
+#[allow(non_camel_case_types)]
+type fuse_fill_dir_t = extern fn(
+    buf: *mut c_void, path: *const c_char, stbuf: *const stat, off: off_t, flags: fuse_fill_dir_flags
+) -> c_int;
+
+#[allow(non_camel_case_types)]
 type fuse_opt_proc_t = extern fn(data: *mut c_void, arg: *const c_char, key: c_int, outargs: *mut fuse_args) -> c_int;
+
+#[repr(C)]
+#[allow(non_camel_case_types)]
+pub struct fuse_operations {
+    pub getattr: extern fn(path: *const c_char, stbuf: *mut stat, fi: *mut fuse_file_info) -> c_int,
+    pub readdir: extern fn(path: *const c_char, buf: *mut c_void, filler: fuse_fill_dir_t, offset: off_t, fi: *mut fuse_file_info, flags: fuse_readdir_flags) -> c_int,
+    pub open: extern fn(path: *const c_char, fi: *mut fuse_file_info) -> c_int,
+    pub read: extern fn(path: *const c_char, buf: *mut c_char, size: size_t, offset: off_t, fi: *mut fuse_file_info) -> c_int,
+}
 
 #[link(name = "fuse")]
 extern "C" {
