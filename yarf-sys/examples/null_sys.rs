a4d420ddf488aad1c53f1e66c71a44502997ba98
Ryo Okubo <syucream1031@gmail.com>
2019-03-09 02:11:35 +0900

    Add null example

--- yarf-sys/examples/null_sys.rs
+++ yarf-sys/examples/null_sys.rs
@@ -6,10 +6,7 @@ use std::ffi::{CStr, CString};
 use std::mem;
 use std::os::raw::{c_char, c_int, c_void};
 use std::ptr;
-use yarf_sys::{fuse_file_info, fuse_fill_dir_t, fuse_operations};
-
-const HELLO_PATH: &str = "/hello";
-const HELLO_CONTENT: &str = "hello, fuse!\n";
+use yarf_sys::{fuse_file_info, fuse_operations};
 
 extern "C" fn yarf_getattr(path: *const c_char, stbuf: *mut stat) -> c_int {
     let path_str = unsafe { CStr::from_ptr(path) };
@@ -18,16 +15,9 @@ extern "C" fn yarf_getattr(path: *const c_char, stbuf: *mut stat) -> c_int {
     match path_slice {
         "/" => {
             unsafe {
-                (*stbuf).st_mode = libc::S_IFDIR | 0o755;
-                (*stbuf).st_nlink = 2;
-            }
-            0
-        }
-        HELLO_PATH => {
-            unsafe {
-                (*stbuf).st_mode = libc::S_IFREG | 0o444;
+                (*stbuf).st_mode = libc::S_IFREG | 0o644;
                 (*stbuf).st_nlink = 1;
-                (*stbuf).st_size = HELLO_CONTENT.len() as i64;
+                (*stbuf).st_size = std::i32::MAX.into();
             }
             0
         }
@@ -35,33 +25,12 @@ extern "C" fn yarf_getattr(path: *const c_char, stbuf: *mut stat) -> c_int {
     }
 }
 
-extern "C" fn yarf_readdir(
-    path: *const c_char,
-    buf: *mut c_void,
-    filler: fuse_fill_dir_t,
-    _offset: off_t,
-    _fi: *mut fuse_file_info,
-) -> c_int {
+extern "C" fn yarf_truncate(path: *const c_char, _offset: off_t) -> c_int {
     let path_str = unsafe { CStr::from_ptr(path) };
     let path_slice = path_str.to_str().unwrap();
 
     match path_slice {
-        "/" => {
-            match filler {
-                Some(filler_func) => unsafe {
-                    let current_dir = CString::new(".").unwrap();
-                    let parent_dir = CString::new("..").unwrap();
-                    let hello_file = CString::new("hello").unwrap();
-
-                    filler_func(buf, current_dir.as_ptr(), ptr::null_mut(), 0);
-                    filler_func(buf, parent_dir.as_ptr(), ptr::null_mut(), 0);
-                    filler_func(buf, hello_file.as_ptr(), ptr::null_mut(), 0);
-                },
-                _ => {}
-            }
-            0
-        }
-
+        "/" => 0,
         _ => -libc::ENOENT,
     }
 }
@@ -71,7 +40,7 @@ extern "C" fn yarf_open(path: *const c_char, _fi: *mut fuse_file_info) -> c_int 
     let path_slice = path_str.to_str().unwrap();
 
     match path_slice {
-        HELLO_PATH => 0,
+        "/" => 0,
         _ => -libc::ENOENT,
     }
 }
@@ -79,6 +48,31 @@ extern "C" fn yarf_open(path: *const c_char, _fi: *mut fuse_file_info) -> c_int 
 extern "C" fn yarf_read(
     path: *const c_char,
     buf: *mut c_char,
+    size: usize,
+    offset: off_t,
+    _fi: *mut fuse_file_info,
+) -> c_int {
+    let path_str = unsafe { CStr::from_ptr(path) };
+    let path_slice = path_str.to_str().unwrap();
+
+    match path_slice {
+        "/" => {
+            if !offset >= std::i32::MAX.into() {
+                unsafe {
+                    libc::memset(buf as *mut c_void, 0, size);
+                }
+                size as c_int
+            } else {
+                0
+            }
+        }
+        _ => -libc::ENOENT,
+    }
+}
+
+extern "C" fn yarf_write(
+    path: *const c_char,
+    _buf: *const c_char,
     _size: usize,
     _offset: off_t,
     _fi: *mut fuse_file_info,
@@ -87,14 +81,7 @@ extern "C" fn yarf_read(
     let path_slice = path_str.to_str().unwrap();
 
     match path_slice {
-        HELLO_PATH => {
-            let content = CString::new(HELLO_CONTENT).unwrap();
-            let content_len = HELLO_CONTENT.len();
-            unsafe {
-                ptr::copy_nonoverlapping(content.as_ptr(), buf, content_len);
-            }
-            content_len as c_int
-        }
+        "/" => 0,
         _ => -libc::ENOENT,
     }
 }
@@ -113,11 +100,11 @@ fn main() {
         link: None,
         chmod: None,
         chown: None,
-        truncate: None,
+        truncate: Some(yarf_truncate),
         utime: None,
         open: Some(yarf_open),
         read: Some(yarf_read),
-        write: None,
+        write: Some(yarf_write),
         statfs: None,
         flush: None,
         release: None,
@@ -127,7 +114,7 @@ fn main() {
         listxattr: None,
         removexattr: None,
         opendir: None,
-        readdir: Some(yarf_readdir),
+        readdir: None,
         releasedir: None,
         fsyncdir: None,
         init: None,
